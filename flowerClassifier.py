b7a40f7d51ec30ffb0e263fb2e9b6c905cf8080f
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-04-23 13:17:10 +0100

    Added code to load the data using DataLoader

--- flowerClassifier.py
+++ flowerClassifier.py
@@ -6,12 +6,23 @@ import torchvision.transforms as transform
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load the dataset - split into training and testing dataset
-# NOTE: Change dataset root to ./data
+# Hyperparameters:
+epochs = 3
+batch_size = 100 # Not sure about this; will almost certainly need changing
+learning_rate = 0.01
+
+
+# Load the dataset - split into training and testing dataset:
 training_dataset = torchvision.datasets.Flowers102(root='./data', split="train", 
                                                    download=True, transform=transform)
 testing_dataset = torchvision.datasets.Flowers102(root='./data', split="test",
                                                    download=True, transform=transform)
-print("This does run!")
 
-# Load the data set using the pytorch data loader 
+
+# Load the data set using the pytorch data loader:
+training_loader = torch.utils.data.DataLoader(training_dataset, batch_size=batch_size, shuffle=True)
+
+testing_loader = torch.utils.data.DataLoader(testing_dataset, batch_size=batch_size, shuffle=False)
+# Don't want to shuffle the test data
+
+print("This does run!")
