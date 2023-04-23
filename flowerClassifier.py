118279bb9ddc4c9828ae887b4c50b628bef2bb53
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-04-23 12:49:56 +0100

    Added .gitignore with 'data' in so the Flowers-102 dataset isn't uploaded to the repo since there are 8000 images

--- flowerClassifier.py
+++ flowerClassifier.py
@@ -1,1 +1,17 @@
-import torch 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transform
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the dataset - split into training and testing dataset
+# NOTE: Change dataset root to ./data
+training_dataset = torchvision.datasets.Flowers102(root='./data', split="train", 
+                                                   download=True, transform=transform)
+testing_dataset = torchvision.datasets.Flowers102(root='./data', split="test",
+                                                   download=True, transform=transform)
+print("This does run!")
+
+# Load the data set using the pytorch data loader 
