dd31b4fe3016c896c627fd3349d47b5d8b5541d5
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-04-25 13:46:44 +0100

    There are errors at the moment but need to push them for everyone to be able to work on it

--- flowerClassifier.py
+++ flowerClassifier.py
@@ -15,12 +15,14 @@ epochs = 3
 batch_size = 10 # Not sure about this; will almost certainly need changing
 learning_rate = 0.01
 
+transformations = trans.Compose([trans.ToTensor(), trans.Resize((500, 500))])
+
 
 # Load the dataset - split into training and testing dataset:
 training_dataset = torchvision.datasets.Flowers102(root='./data', split="train", 
-                                                   download=True, transform=trans.ToTensor())
+                                                   download=True, transform=transformations)
 testing_dataset = torchvision.datasets.Flowers102(root='./data', split="test",
-                                                   download=True, transform=trans.ToTensor())
+                                                   download=True, transform=transformations)
 # We use trans.ToTensor() to transform the input data into pytorch tensors of the form: [num_channels, image_height, image_width], e.g. [3, 597, 500] 
 
 
@@ -46,26 +48,21 @@ class Flowers_CNN(nn.Module):
         super(Flowers_CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(2,2),
                                 stride=2, padding=(1,1)) # (1,1) padding means the image sizes don't decrease
-        self.fully_connected1 = nn.Linear(16*625*500, 1000) 
+        self.fully_connected1 = nn.Linear(1008016, 10) 
         # in_features = (num channels (16, from the out_channels of conv1 above) x image height x image width) = 16*625*500
-        self.fully_connected2 = nn.Linear(1000, 300) 
-        self.fully_connected3 = nn.Linear(300, 102) # output_features=102 because there are 102 classes
+        # self.fully_connected2 = nn.Linear(1000, 300) 
+        # self.fully_connected3 = nn.Linear(300, 102) # output_features=102 because there are 102 classes
 
     def forward(self, val):
         val = F.relu(self.conv1(val))
-        val = val.view(-1, 16*625*500) # This is to flatten the data for the fully connected layers. Could use  torch.flatten(val, 1) instead I think
-        val = F.relu(self.fully_connected1(val))
-        val = F.relu(self.fully_connected2(val))
-        val = F.relu(self.fully_connected3(val))
-        val = self.fc3(val)
+        val = F.relu(self.fully_connected1(val.view(-1, 1008016))) 
+        # val = F.relu(self.fully_connected2(val))
+        # val = F.relu(self.fully_connected3(val))
         return val
         
 
 neural_net = Flowers_CNN().to(device)
 loss_function = nn.MSELoss()
-
-# print(list((neural_net.parameters())))
-# print(neural_net)
 
 
 # Training loop:
@@ -74,8 +71,12 @@ for ep in range(epochs): # Each iteration is a forward pass to train the data
         images = images.to(device)
         image_labels = image_labels.to(device)
 
-        label_pred = neural_net(images)
-        loss = loss_function(label_pred, image_labels)
+        label_pred = neural_net(images) 
+        print(label_pred[0])
+        print(image_labels)
+        loss = loss_function(label_pred, image_labels) 
+        # Error on line 83: The size of tensor a (102) must match the size of tensor b (10) at non-singleton dimension 3
+    
 
         loss.backward()
 
