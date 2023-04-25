f9b390866c73c268ff2cd47bac3cdbcbaec4de87
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-04-25 20:39:04 +0100

    The neural network now trains corretly!

--- flowerClassifier.py
+++ flowerClassifier.py
@@ -12,7 +12,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters:
 epochs = 3
-batch_size = 102 # Not sure about this; will almost certainly need changing
+batch_size = 4 # Not sure about this; will almost certainly need changing
 learning_rate = 0.01
 
 transformations = trans.Compose([trans.ToTensor(), trans.Resize((500, 500))])
@@ -39,26 +39,27 @@ classes = np.arange(0, 102) # Creates array of numbers from 0 to 102 (exclusive 
 # Define the CNN:
 class Flowers_CNN(nn.Module): 
     def __init__(self):
-        # Add a pooling layer? 
-        # Add another convolutional layer?
         super(Flowers_CNN, self).__init__()
+        self.flatten = nn.Flatten()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(2,2),
-                                stride=2, padding=(1,1)) # (1,1) padding means the image sizes don't decrease
-        self.fully_connected1 = nn.Linear(1008016, 102) 
-        # in_features = (num channels (16, from the out_channels of conv1 above) x image height x image width) = 16*625*500
+                                stride=1, padding=(1,1)) # (1,1) padding means the image sizes don't decrease
+        self.fully_connected1 = nn.Linear(4016016, 102) # input_features was 500*500*16 but needs to be 4016016 I think
+        # in_features = (num channels (16, from the out_channels of conv1 above) x image height x image width) = 16*500*500
         # self.fully_connected2 = nn.Linear(1000, 300) 
         # self.fully_connected3 = nn.Linear(300, 102) # output_features=102 because there are 102 classes
 
     def forward(self, val):
         val = F.relu(self.conv1(val))
-        val = F.relu(self.fully_connected1(val.view(-1, 1008016))) 
+        val = self.flatten(val)
+        val = F.relu(self.fully_connected1(val))
         # val = F.relu(self.fully_connected2(val))
         # val = F.relu(self.fully_connected3(val))
         return val
         
 
 neural_net = Flowers_CNN().to(device)
-loss_function = nn.MSELoss()
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(neural_net.parameters(), lr=learning_rate) 
 
 
 # Training loop:
@@ -66,13 +67,14 @@ for ep in range(epochs): # Each iteration is a forward pass to train the data
     for i, (images, image_labels) in enumerate(training_loader):
         images = images.to(device)
         image_labels = image_labels.to(device)
+        # print(images.shape, "and", image_labels.shape)
 
         label_pred = neural_net(images) 
-        print(label_pred[0])
-        print(image_labels)
-        loss = loss_function(label_pred[0], image_labels) 
+        loss = loss_function(label_pred, image_labels) 
+        # print(loss)
 
-        loss.backward() # This just doesn't work at the moment
+        optimizer.zero_grad()
+        loss.backward() 
+        optimizer.step()
 
-        if (i+1) % 2000 == 0:
-            print("Epoch Number =" + ep + ", Loss =" + loss)
+        print("Epoch Number = " + str(ep) + ", Index =", str(i), "/254", "Loss = " + str(loss))
