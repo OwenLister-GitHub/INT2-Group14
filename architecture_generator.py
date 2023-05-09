9c4ebe371a2617b7b28b83be5a5659c80f431e7c
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-05-09 22:44:23 +0100

    Edited code to generate latest architecturer and the actual architecture .png

--- architecture_generator.py
+++ architecture_generator.py
@@ -23,10 +23,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyperparameters:
 epochs = 80
 batch_size = 64
-learning_rate = 0.00001 
+learning_rate = 0.0004
 
 transformations1 = trans.Compose([trans.ToTensor(), 
-                                 trans.Resize((100,100))])
+                                 trans.Resize((250, 250))])
                                  
 
 
@@ -45,25 +45,11 @@ testing_loader = torch.utils.data.DataLoader(testing_dataset, batch_size=batch_s
 
 
 
-
-# Transform Dataset with trans.Normalise :
-mean = 0.
-std = 0.
-for images, _ in training_loader:
-    batch_samples = images.size(0) # batch size (the last batch can have smaller size!)
-    images = images.view(batch_samples, images.size(1), -1)
-    mean += images.mean(2).sum(0)
-    std += images.std(2).sum(0)
-
-mean /= len(training_loader.dataset)
-std /= len(training_loader.dataset)
-
 transformations2 = trans.Compose([trans.ToTensor(), 
                                   trans.RandomHorizontalFlip(0.2),
                                   trans.RandomVerticalFlip(0.2),
                                   trans.RandomRotation(55),
-                                  trans.Resize((100,100)),
-                                  trans.Normalize(mean=mean, std=std)])
+                                  trans.Resize((250, 250))])
 
 
 
@@ -87,32 +73,52 @@ classes = np.arange(0, 102) # Creates array of numbers from 0 to 102 (exclusive 
  
 
 # Define the CNN:
-class Flowers_CNN(nn.Module): 
+class Flowers_CNN(nn.Module):
     def __init__(self):
         super(Flowers_CNN, self).__init__()
         self.flatten = nn.Flatten()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=5,
-                                stride=(1,1), padding=(1,1)) 
-        self.conv2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=5,
-                                stride=(1,1), padding=(1,1))
-        self.conv3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=5,
-                                stride=(1,1), padding=(1,1))
-        self.batch_layer1=nn.BatchNorm2d(256)
-        self.batch_layer2=nn.BatchNorm2d(256)
-        self.batch_layer3=nn.BatchNorm2d(256)
-        self.pool1 = nn.MaxPool2d(3,3)
-        self.fully_connected1 = nn.Linear(16384, 102)
-        # in_features = (num channels (16, from the out_channels of conv1 above) x image height x image width) 
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3,
+                               stride=(1,1), padding=(1,1))
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3,
+                               stride=(1,1), padding=(1,1))
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3,
+                               stride=(1,1), padding=(1,1))
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3,
+                               stride=(1,1), padding=(1,1))
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3,
+                               stride=(1,1), padding=(1,1))
+        self.conv6 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3,
+                               stride=(1,1), padding=(1,1))
+        self.batch_layer1=nn.BatchNorm2d(16)
+        self.batch_layer2=nn.BatchNorm2d(32)
+        self.batch_layer3=nn.BatchNorm2d(64)
+        self.batch_layer4=nn.BatchNorm2d(128)
+        self.batch_layer5=nn.BatchNorm2d(256)
+        self.batch_layer6=nn.BatchNorm2d(256)
+
+        self.paramater_relu = nn.PReLU()
+        self.pool1 = nn.MaxPool2d(2,2)
+        self.fully_connected1 = nn.Linear(12544, 102)
+        # in_features = (num channels (16, from the out_channels of conv1 above) x image height x image width)
 
     def forward(self, val):
         val = self.conv1(val)
-        val = F.relu(self.batch_layer1(val))
+        val = self.paramater_relu(self.batch_layer1(val))
         val = self.pool1(val)
         val = self.conv2(val)
-        val = F.relu(self.batch_layer2(val))
-        val = self.pool1(val)        
+        val = self.paramater_relu(self.batch_layer2(val))
+        val = self.pool1(val)
         val = self.conv3(val)
-        val = F.relu(self.batch_layer3(val))
+        val = self.paramater_relu(self.batch_layer3(val))
+        val = self.pool1(val)
+        val = self.conv4(val)
+        val = self.paramater_relu(self.batch_layer4(val))
+        val = self.pool1(val)
+        val = self.conv5(val)
+        val = self.paramater_relu(self.batch_layer5(val))
+        val = self.pool1(val)
+        val = self.conv6(val)
+        val = self.paramater_relu(self.batch_layer6(val))
         val = self.flatten(val)
         val = self.fully_connected1(val)
         return val
@@ -139,6 +145,6 @@ class Flowers_CNN(nn.Module):
 # dot.render('neural_net_architecture', format='png')
 
 
-model_graph = draw_graph(Flowers_CNN(), input_size=(1, 3, 100, 100), expand_nested=True)
+model_graph = draw_graph(Flowers_CNN(), input_size=(1, 3, 250, 250), expand_nested=True)
 model_graph.resize_graph(scale=5.0)
 model_graph.visual_graph
